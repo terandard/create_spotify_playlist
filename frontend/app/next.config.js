5dd7c2295648429ac06a37e3b85b0c6b2b96b348
terandard <terandard0218@gmail.com>
2022-06-29 23:09:16 +0900

    nextjsのホットリロード追加

--- frontend/app/next.config.js
+++ frontend/app/next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+}
