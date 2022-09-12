76c193f10ac7d23aa3d6ff6ce29d2a6a0b118fcb
terandard <terandard0218@gmail.com>
2022-09-12 17:40:07 +0900

    プレイリストの型定義

---
+++ frontend/app/types/playlist.tsx
@@ -0,0 +1,50 @@
+export interface SpotifyPlaylist {
+    collaborative: boolean,
+    description: string,
+    external_urls: {
+        spotify: string
+    },
+    followers: {
+        href: string,
+        total: number
+    },
+    href: string,
+    id: string,
+    images: [
+        {
+            url: string,
+            height: number,
+            width: number
+        }
+    ],
+    name: string,
+    owner: {
+        external_urls: {
+            spotify: string
+        },
+        followers: {
+            href: string,
+            total: number
+        },
+        href: string,
+        id: string,
+        type: string,
+        uri: string,
+        display_name: string
+    },
+    public: boolean,
+    snapshot_id: string,
+    tracks: {
+        href: string,
+        items: [
+            {}
+        ],
+        limit: number,
+        next: string,
+        offset: number,
+        previous: string,
+        total: number
+    },
+    type: string,
+    uri: string
+}
