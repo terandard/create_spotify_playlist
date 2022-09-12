c116e853f081bc4865feb36e351dcd62eaf32420
terandard <terandard0218@gmail.com>
2022-09-12 18:32:05 +0900

    userページの型修正

---
+++ frontend/app/pages/user/index.tsx
@@ -0,0 +1,47 @@
+import { withIronSessionSsr } from "iron-session/next";
+import { ironOptions } from "../../lib/config";
+import { SpotifyApi } from "../../lib/SpotifyApi";
+import { UserInfo } from "../../types/userinfo";
+
+type PropsData = {
+    userInfo: UserInfo
+}
+
+export default function ShowUserInfo(props: PropsData) {
+    var user_info: UserInfo = props.userInfo;
+    return (
+        <main>
+            <h1>Logged in as {user_info.display_name}</h1>
+            <div className="media">
+                <div className="pull-left">
+                    <img className="media-object" width="150" src="{{images.0.url}}" />
+                </div>
+                <div className="media-body">
+                    <dl className="dl-horizontal">
+                        <dt>Display name</dt><dd className="clearfix">{user_info.display_name}</dd>
+                        <dt>Id</dt><dd>{user_info.id}</dd>
+                        <dt>Email</dt><dd>{user_info.email}</dd>
+                        <dt>Spotify URI</dt><dd><a href="{external_urls.spotify}">{user_info.external_urls.spotify}</a></dd>
+                        <dt>Link</dt><dd><a href="{{href}}">{user_info.href}</a></dd>
+                        <dt>Profile Image</dt><dd className="clearfix"><a href="{{images.0.url}}"></a></dd>
+                        <dt>Country</dt><dd>{user_info.country}</dd>
+                    </dl>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export const getServerSideProps = withIronSessionSsr(
+    async function getServerSideProps({ req }) {
+      const user = req.session.user;
+      const spotifyApi = new SpotifyApi(user.accessToken);
+      const userinfo = await spotifyApi.getUserInfo();
+
+      const props: PropsData = {
+        userInfo: userinfo,
+      }
+
+      return { props: props }
+    }, ironOptions
+  );
