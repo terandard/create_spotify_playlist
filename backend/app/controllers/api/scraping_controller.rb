d4e109ddf5c4b035beb4bd61f75dbe1f4686dcac
terandard <terandard0218@gmail.com>
2022-09-19 23:36:29 +0900

    apiリクエスト時のパラメータ確認追加

--- backend/app/controllers/api/scraping_controller.rb
+++ backend/app/controllers/api/scraping_controller.rb
@@ -1,7 +1,14 @@
 class Api::ScrapingController < ApplicationController
+    before_action :check_param
     def index
         # e.g. params[:url] = events/1420946
         result = Playlist.new(params[:url]).to_json
         render json: {status: :ok, data: result}
+    end
+
+    def check_param
+        if !params.has_key?(:url)
+            render json: {status: :bad_request}
+        end
     end
 end
