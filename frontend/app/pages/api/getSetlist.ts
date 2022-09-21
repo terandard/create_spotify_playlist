260950aad5c71c2089c0fcbca1b62d1e1ea9c7a8
terandard <terandard0218@gmail.com>
2022-09-21 12:39:53 +0900

    スクレイピング結果から曲を検索

--- frontend/app/pages/api/getSetlist.ts
+++ frontend/app/pages/api/getSetlist.ts
@@ -1,10 +1,49 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { SpotifyApi } from '../../lib/SpotifyApi';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { ironOptions } from '../../lib/config';
+import { RubyScrapingResponse } from '../../types/ruby-api';
+import { SpotifyApiTrack, Track } from '../../types/track';
+import { PlaylistFromScraping } from '../../types/playlist';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler =  async (req: NextApiRequest, res: NextApiResponse) => {
   const query = req.query;
   const { url } = query;
   const response = await fetch('http://ruby-api:8080/api/scraping?url='+url)
   const result = await response.json()
-  console.log(result)
-  res.status(200).json(result)
+
+  const data: RubyScrapingResponse = result.data;
+  const access_token: string = req.session.user.accessToken;
+  const spotifyApi = new SpotifyApi(access_token);
+  let search_query: string;
+
+  // 取得した曲を検索
+  let tracks: Array<Track> = await Promise.all(data.tracks.map(async (t) => {
+    search_query = "q=track:" + t + "+artist:" + data.artist + "&type=track";
+    let search_result: Array<SpotifyApiTrack> = await spotifyApi.searchItem(search_query);
+    let first_track: Track = pickupTrackFromApi(search_result[0])
+    return first_track
+  }))
+
+  const setlist_result: PlaylistFromScraping = {
+    event_title: data.event_title,
+    event_subtitle: data.event_subtitle,
+    tracks: tracks
+  }
+  console.log(setlist_result)
+
+  res.status(200).json(setlist_result)
 }
+
+function pickupTrackFromApi (ApiTrack: SpotifyApiTrack): Track {
+  return {
+    album: ApiTrack.album,
+    artists: ApiTrack.artists,
+    id: ApiTrack.id,
+    name: ApiTrack.name,
+    href: ApiTrack.href,
+    uri: ApiTrack.uri
+  }
+}
+
+export default withIronSessionApiRoute(handler, ironOptions)
