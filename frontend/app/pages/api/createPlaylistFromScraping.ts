26cfa2acca231592970e30d142218bbc262e2c4b
terandard <terandard0218@gmail.com>
2022-09-21 14:40:57 +0900

    スクレイピング結果からプレイリスト作成

---
+++ frontend/app/pages/api/createPlaylistFromScraping.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { SpotifyApi } from '../../lib/SpotifyApi';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { ironOptions } from '../../lib/config';
+import { SpotifyPlaylist, CreateSpotifyPlaylistParam, AddItemToSpotifyPlaylistParam } from '../../types/playlist';
+import { UserInfo } from "../../types/userinfo";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const access_token: string = req.session.user.accessToken;
+    const spotifyApi = new SpotifyApi(access_token);
+    const userinfo: UserInfo = await spotifyApi.getUserInfo();
+
+    const create_param: CreateSpotifyPlaylistParam = {
+        user_id: userinfo.id,
+        playlist_name: req.body.playlistTitle,
+        public: true,
+        collaborative: false,
+        description: req.body.playlistDescription
+    }
+
+    let playlist_id: string;
+    spotifyApi.createUserPlaylist(create_param)
+    .then((playlist: SpotifyPlaylist) => {
+        console.log(playlist)
+        playlist_id = playlist.id;
+        const add_param: AddItemToSpotifyPlaylistParam = {
+            playlist_id: playlist_id,
+            position: 0,
+            uris: req.body.uris
+        }
+        return spotifyApi.addItemToPlaylist(add_param)
+    })
+    .then((r) => res.status(200).json({playlist_id: playlist_id}))
+
+}
+
+export default withIronSessionApiRoute(handler, ironOptions)
--- frontend/app/pages/get_setlist.tsx
+++ frontend/app/pages/get_setlist.tsx
@@ -1,10 +1,14 @@
 import React, {useState, FormEvent} from "react";
 import { PlaylistFromScraping } from "../types/playlist";
+import { useRouter } from 'next/router';
 
 export default function GetSetlist() {
     const [scrapingData, setScrapingData] = useState<PlaylistFromScraping>();
     const [url, setUrl] = useState<string>("");
     const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [playlistTitle, setPlaylistTitle] = useState<string>("")
+    const [playlistDescription, setPlaylistDescription] = useState<string>("")    
+    const router = useRouter();
 
     const fetchItems = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -16,10 +20,42 @@ export default function GetSetlist() {
 
         fetch(api_url)
         .then((res) => res.json())
-        .then((res) => {
+        .then((res: PlaylistFromScraping) => {
             setScrapingData(res);
+            setPlaylistTitle(res.event_title + res.event_subtitle)
             setIsLoading(false);
         })
+    }
+
+    const createPlaylist = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+        
+        const uris = scrapingData?.tracks.map((t) => {
+            return t.uri
+        });
+
+        console.log(uris)
+
+        const options = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                playlistTitle: playlistTitle,
+                playlistDescription: playlistDescription,
+                uris: uris
+            }),
+        }
+
+        const response = await fetch('/api/createPlaylistFromScraping', options);
+        const result = await response.json();
+        console.log(result);
+
+        router.push("/playlist/" + result.playlist_id)
+
+        setIsLoading(false);
     }
 
     return (
@@ -33,6 +69,15 @@ export default function GetSetlist() {
             <div>
                 <p>Event title : {scrapingData?.event_title}</p>
                 <p>Event subtitle : {scrapingData?.event_subtitle}</p>
+
+                <form onSubmit={(e) => createPlaylist(e)}>
+                    <label>Playlist title</label>
+                    <input type="text" value={playlistTitle} onChange={(e) => setPlaylistTitle(e.target.value)} />
+                    <label>Playlist description</label>
+                    <input type="text" value={playlistDescription} onChange={(e) => setPlaylistDescription(e.target.value)} />
+                    <button type="submit">Create playlist</button>
+                </form>
+
                 {scrapingData?.tracks.map((t, index) => (
                     <p key={index}>{index+1}. {t.name}</p>
                 ))}
