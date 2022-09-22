c2388730b97c122711a07455df8be0055cc2a5d9
terandard <terandard0218@gmail.com>
2022-09-22 16:54:52 +0900

    material uiのimport方法変更

--- frontend/app/components/loading.tsx
+++ frontend/app/components/loading.tsx
@@ -1,4 +1,5 @@
-import { CircularProgress, Box } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 type Props = {
     isLoading: boolean
