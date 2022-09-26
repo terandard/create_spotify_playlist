f6406ba9bd2b5998b5cab6b960ed0296904a96c7
terandard <terandard0218@gmail.com>
2022-09-26 23:23:47 +0900

    エラーページ追加

---
+++ frontend/app/pages/401.tsx
@@ -0,0 +1,9 @@
+const Custom401 = () => {
+    return (
+        <div>
+            <p>セッションの有効期限が切れました。</p>
+        </div>
+    )
+}
+
+export default Custom401
--- frontend/app/pages/_app.tsx
+++ frontend/app/pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout';
-
+import Custom401 from './401';
+import Error from './_error';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (pageProps.error) {
+    switch(pageProps.error.statusCode) {
+      case 401:
+        return <Custom401 />
+      default:
+        return <Error statusCode={pageProps.error.statusCode}/>
+    }
+  }
   return (
     <Layout>
       <Component {...pageProps} />
