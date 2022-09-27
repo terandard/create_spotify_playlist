a08da4f0f603aaa9f09726a1eee1d71be0ad9e98
terandard <terandard0218@gmail.com>
2022-09-27 19:26:05 +0900

    401ページでアクセストークンを再取得する

--- frontend/app/pages/401.tsx
+++ frontend/app/pages/401.tsx
@@ -1,7 +1,24 @@
+import React, {useState} from "react";
+import Loading from "../components/loading";
+import { useRouter } from 'next/router'
+
 const Custom401 = () => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const refreshToken = async () => {
+        setIsLoading(true);
+        const response = await fetch('/api/auth/refreshToken');
+        router.back();
+        setIsLoading(false);
+    }
+
     return (
         <div>
+            <Loading isLoading={isLoading} />
             <p>セッションの有効期限が切れました。</p>
+            <p>再認証してください。</p>
+            <button onClick={() => refreshToken()}>再認証</button>
         </div>
     )
 }
