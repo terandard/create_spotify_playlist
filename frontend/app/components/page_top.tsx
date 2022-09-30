1be84fababb3c165432b75bdb2edee2400b5fe3f
terandard <terandard0218@gmail.com>
2022-09-30 16:20:22 +0900

    スクロール前はトップへ戻るボタンを非表示

--- frontend/app/components/page_top.tsx
+++ frontend/app/components/page_top.tsx
@@ -1,10 +1,36 @@
 import styles from '../styles/PageTop.module.scss';
+import { useState, useEffect } from 'react';
+
+const PAGE_Y_OFFSET: number = 200
 
 const PageTop = () => {
+    const [show, setShow] = useState<boolean>(false)
 
-    return (
-        <a href="#" className={styles.top} />
+    const changeShow = () => {
+        setInterval(()=> {
+            if (window.pageYOffset > PAGE_Y_OFFSET) {
+                setShow(true)
+            } else {
+                setShow(false)
+            }
+            return 1
+        }, 500)
+    }
+    
+    const onScrollTop = () => {
+        window.scroll({ top: 0, behavior: 'smooth' })
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeShow)
+        return () => window.removeEventListener('scroll', changeShow)
+    }, [])
+    
+    if (show) return (
+        <button className={styles.top} onClick={onScrollTop} />
     )
+
+    else return null
 }
 
 export default PageTop;
