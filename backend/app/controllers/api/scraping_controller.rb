d1aedbbdb33fa6736581347b1a8a24e83b6ae973
terandard <terandard0218@gmail.com>
2023-08-20 17:12:53 +0900

    Run rubocop -A

--- backend/app/controllers/api/scraping_controller.rb
+++ backend/app/controllers/api/scraping_controller.rb
@@ -1,14 +1,18 @@
-class Api::ScrapingController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  class ScrapingController < ApplicationController
     before_action :check_param
     def index
-        # e.g. params[:url] = events/1420946
-        playlist = Playlist.new(params[:url])
-        render json: {status: playlist.get_status, data: playlist.to_json}
+      # e.g. params[:url] = events/1420946
+      playlist = Playlist.new(params[:url])
+      render json: { status: playlist.get_status, data: playlist.to_json }
     end
 
     def check_param
-        if !params.has_key?(:url)
-            render json: {status: :bad_request}
-        end
+      return if params.key?(:url)
+
+      render json: { status: :bad_request }
     end
+  end
 end
--- backend/app/models/playlist.rb
+++ backend/app/models/playlist.rb
@@ -1,60 +1,62 @@
+# frozen_string_literal: true
+
 require 'selenium-webdriver'
 
 class Playlist
-    def initialize(url)
-        @url = 'https://www.livefans.jp/' + url
-        @status = :ok
-        scraping
+  def initialize(url)
+    @url = "https://www.livefans.jp/#{url}"
+    @status = :ok
+    scraping
+  end
+
+  def scraping
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--blink-settings=imageEnabled=false')
+
+    driver = Selenium::WebDriver.for(:chrome, options:)
+    driver.get(@url)
+    track_elements = driver.find_elements(:css, '.rnd')
+
+    if track_elements.blank?
+      @status = :not_found
+      driver.quit
+      return
     end
 
-    def scraping
-        options = Selenium::WebDriver::Chrome::Options.new
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--blink-settings=imageEnabled=false')
+    parent_element = driver.find_element(:css, '.setBlock')
+    @artist = parent_element.find_element(:id, 'eventEventArtistName').attribute('value')
+    @event_title = parent_element.find_element(:id, 'eventTitle').attribute('value')
+    @event_subtitle = parent_element.find_element(:id, 'eventSubTitle').attribute('value')
 
-        driver = Selenium::WebDriver.for :chrome, options: options
-        driver.get(@url)
-        track_elements = driver.find_elements(:css, '.rnd')
+    @tracks = extract_tracks(track_elements)
+    driver.quit
+  end
 
-        if track_elements.blank?
-            @status = :not_found
-            driver.quit
-            return
-        end
-
-        parent_element = driver.find_element(:css, '.setBlock')
-        @artist = parent_element.find_element(:id, 'eventEventArtistName').attribute("value")
-        @event_title = parent_element.find_element(:id, 'eventTitle').attribute("value")
-        @event_subtitle = parent_element.find_element(:id, 'eventSubTitle').attribute("value")
-
-        @tracks = extract_tracks(track_elements)
-        driver.quit
+  def extract_tracks(track_elements)
+    re = Regexp.new('top: (\d+)px')
+    tracks = {}
+    track_elements.each do |e|
+      key = re.match(e.attribute('style'))[1].to_i
+      track = e.find_element(:class, 'ttl').attribute('textContent')
+      tracks[key] = track
     end
 
-    def extract_tracks(track_elements)
-        re = Regexp.new('top: (\d+)px')
-        tracks = {}
-        track_elements.each do |e|
-            key = re.match(e.attribute('style'))[1].to_i
-            track = e.find_element(:class, 'ttl').attribute("textContent")
-            tracks[key] = track
-        end
+    tracks.sort.to_h.values
+  end
 
-        tracks.sort.to_h.values
-    end
+  def to_json(*_args)
+    {
+      artist: @artist,
+      event_title: @event_title,
+      event_subtitle: @event_subtitle,
+      tracks: @tracks
+    }
+  end
 
-    def to_json
-        {
-            artist: @artist,
-            event_title: @event_title,
-            event_subtitle: @event_subtitle,
-            tracks: @tracks
-        }
-    end
-
-    def get_status
-        @status
-    end
+  def get_status
+    @status
+  end
 end
